8fce01241bfd2f5af7889c195bab66396cfef0a0
Tillmann Vogt <tillk.vogt@googlemail.com>
2014-04-20 22:01:34 +0200

    Update test.hs

--- test.hs
+++ test.hs
@@ -9,7 +9,6 @@ import Filesystem.Path.CurrentOS
 import Diagrams.SVG.Attributes (PreserveAR(..), AlignSVG(..), Place(..), MeetOrSlice(..))
 
 main = do
-    -- args <- getArgs
     let min = PAR (AlignXY PMid PMin) Meet
     let mid = PAR (AlignXY PMid PMid) Meet
     let max = PAR (AlignXY PMax PMax) Meet
@@ -23,18 +22,6 @@ main = do
     diagram7FromSVG <- readSVGFile "svgs/web.svg" 100 100                    min
     diagram8FromSVG <- readSVGFile "svgs/web.svg" 100 100                    mid
 
---    diagram3FromSVG <- readSVGFile "VD_11_Filled_00.svg" 100 100        max -- nearly works
---    diagram2FromSVG <- readSVGFile "star.svg" 100 100                   max -- works
---    diagram9FromSVG <- readSVGFile "ge.svg" 100 100 (PAR (AlignXY PMin PMin) Meet) -- no fill
---    diagram10FromSVG <- readSVGFile "money-bag-3.svg" 100 100 (PAR (AlignXY PMin PMin) Meet) -- no fill
---    diagram11FromSVG <- readSVGFile "KFC_logo.svg" 100 100 (PAR (AlignXY PMin PMin) Meet) -- black rect in front
---    diagram12FromSVG <- readSVGFile "ubuntu.svg" 100 100 (PAR (AlignXY PMin PMin) Meet) -- no black circle
---    diagram13FromSVG <- readSVGFile "peace.svg" 100 100 (PAR (AlignXY PMin PMin) Meet) -- bug
---    diagram14FromSVG <- readSVGFile "Bundesliga-Logo.svg" 100 100 (PAR (AlignXY PMin PMin) Meet) -- bug
-    -- diagram8FromSVG <- readSVGFile "lhc.svg" 100 100 (PAR (AlignXY PMin PMin) Meet) -- (decodeString (head args))
-    -- diagram9FromSVG <- readSVGFile "Wikipedia-logo.svg" -- parse bug
-                                   -- "partition.svg" -- bug
-                                   -- "money-bag-3.svg" -- empty
     mainWith $
       (diagram0FromSVG ||| diagram1FromSVG ||| diagram2FromSVG) ===
       ( (diagram3FromSVG === diagram4FromSVG) |||
