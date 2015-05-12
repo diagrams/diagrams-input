21b58f8bfed86e0a96865848c680d465027a638e
Tillmann Vogt <tillk.vogt@googlemail.com>
2015-05-12 09:58:19 +0200

    cleanup

--- src/Diagrams/TwoD/Input.hs
+++ src/Diagrams/TwoD/Input.hs
@@ -1,15 +1,9 @@
-{-# LANGUAGE EmptyDataDecls        #-}
 {-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE UndecidableInstances  #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE ConstraintKinds #-}
 
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Diagrams.TwoD.Input
--- Copyright   :  (c) 2015 diagrams-lib team (see LICENSE)
+-- Copyright   :  (c) 2015 Tillmann Vogt
 -- License     :  BSD-style (see LICENSE)
 -- Maintainer  :  diagrams-discuss@googlegroups.com
 --
@@ -21,38 +15,32 @@ module Diagrams.TwoD.Input
     , loadImageExternal
     ) where
 
-import           Control.Monad -- mplus
+import           Control.Monad (msum)
 import           Codec.Picture
 import           Codec.Picture.Types  (dynamicMap)
 
-import           Data.Colour          (AlphaColour)
 import           Data.Semigroup
 import           Data.Typeable        (Typeable)
 
 import           Diagrams.Core
-
-import           Diagrams.Attributes  (colorToSRGBA)
 import           Diagrams.TwoD.Image
-import           Diagrams.TwoD.Shapes (rect)
 import           Diagrams.TwoD.Size
 import           Diagrams.TwoD.Types
-import           Diagrams.SVG.ReadSVG (readSVGFile, InputConstraints, dataUriToImage)
-import           Linear.Affine
--- import           Filesystem.Path (FilePath)
-import           Filesystem.Path.CurrentOS (encodeString, decodeString)
--- import           Prelude hiding (FilePath)
+import           Diagrams.SVG.ReadSVG (readSVGFile, InputConstraints)
+
+import           Filesystem.Path.CurrentOS (decodeString)
 
 -- | Load 2d formats given by a filepath and embed them
 loadImageEmbedded :: InputConstraints b n => FilePath -> IO (Either String (QDiagram b V2 n Any))
 loadImageEmbedded path = do
-  dImg <- readImage path -- FilePath -> IO (Either String DynamicImage)
-  svgImg <- readSVGFile (decodeString path) -- FilePath -> IO (Either String (Diagram b))
+  dImg <- readImage path
+  svgImg <- readSVGFile (decodeString path)
   return $ msum  [ svgImg,
                    fmap (image.rasterImage) dImg ] -- skip "Left"s and use the first "Right" image
   where
     rasterImage img = DImage (ImageRaster img) (dynamicMap imageWidth img) (dynamicMap imageHeight img) mempty
 
--- | Load 2d formats given by a filepath and make a reference if the format allows it
+-- | Load 2d formats given by a filepath and make a reference
 loadImageExternal :: (InputConstraints b n, Renderable (DImage n External) b) => FilePath -> IO (Either String (QDiagram b V2 n Any))
 loadImageExternal path = do
   dImg <- readImage path
